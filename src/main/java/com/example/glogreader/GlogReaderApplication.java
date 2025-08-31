aee2b30b740ab5dad83b659dbf310e7ba698bd58
WESLEY CARLOS NASCIMENTO <wesley.nascimento@bradesco.com.br>
2025-08-30 21:16:20 -0300

    Deploy in Azure as war.

--- src/main/java/com/example/glogreader/GlogReaderApplication.java
+++ src/main/java/com/example/glogreader/GlogReaderApplication.java
@@ -2,9 +2,17 @@ package com.example.glogreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GlogReaderApplication {
+public class GlogReaderApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GlogReaderApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(GlogReaderApplication.class, args);
     }
